b1b37e1451425592e7f15ace2da2a31e5dbb342b
Gaëtan LAGIER <gaetan@bimdata.io>
2022-10-20 14:30:19 +0200

    provide zoneId to content component (#33)

--- src/renderer/contentManager/contentManager.js
+++ src/renderer/contentManager/contentManager.js
@@ -47,7 +47,7 @@ function makeContentManager(renderer) {
             node.id,
             Object.assign({}, this.getContent(contentName), {
               ref: renderer.vue.ref(null),
-              options: node.options,
+              options: { zoneId: node.id, ...node.options },
               name: contentName,
             })
           );
